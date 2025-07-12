640aa82680c5f0e9cc54bbed447afa4ee02f9e51
Mlshl <mishi.beraia@gmail.com>
2025-07-13 00:35:58 +0400

    Frontend structure changes and slight refactor, preparation for future registration/login pages

---
+++ frontend/src/api/taskApi.jsx
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const BASE_URL = 'https://localhost:8080/tasks'
+
+export const createTask = (task) => axios.post(BASE_URL, task);
+export const updateTask = (task) => axios.put('${BASE_URL}/${task_id}', task);
+export const getAllTasks = () => axios.get(BASE_URL)
