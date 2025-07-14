79968cebfbc524c471a7121380d495ba655a39c1
Mlshl <mishi.beraia@gmail.com>
2025-07-14 18:25:36 +0400

    Fixed reordering rare bug

--- frontend/src/api/taskApi.jsx
+++ frontend/src/api/taskApi.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const BASE_URL = 'http://localhost:8080/tasks'
 
+export const findTaskById = (id) => axios.get(`${BASE_URL}/${id}`);
 export const createTask = (task) => axios.post(BASE_URL, task);
 export const updateTask = (task) => axios.put(`${BASE_URL}/${task.id}`, task);
 export const getAllTasks = () => axios.get(BASE_URL)
